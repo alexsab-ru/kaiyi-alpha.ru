8b99b19a8becaae90315cfec46b9f54019f3eadf
Alexander Sabanov <alexsab@mail.ru>
2025-01-31 16:23:52 +0700

    upd cars.xml, add used_cars

--- src/const.js
+++ src/const.js
@@ -29,7 +29,7 @@ export const LINK_WIDGET_ORGNIZATION = 'CDV3UKpN';
 export const LINKS_MENU = [
 	{url: 'cars/', name: 'Авто в наличии'},
 	// {url: 'catalog/', name: 'Каталог'},
-	// {url: 'used_cars/', name: 'Авто с пробегом'},
+	{url: 'used_cars/', name: 'Авто с пробегом'},
 	{url: 'models/', name: 'Модели'},
 	// {url: 'trade-in/', name: 'Оценка автомобиля'},
 	// {url: 'special-offers/', name: 'Спецпредложения'},
